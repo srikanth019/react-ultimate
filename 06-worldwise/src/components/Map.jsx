6e1038abc6f7a999bca3d984bf89cfacd12e9b95
srikanth019 <srikanth.golla@brainvire.com>
2024-04-19 12:52:15 +0530

    Till sec-17 SPA

--- 06-worldwise/src/components/Map.jsx
+++ 06-worldwise/src/components/Map.jsx
@@ -1,7 +1,19 @@
 import styles from './Map.module.css'
+import { useNavigate, useSearchParams } from "react-router-dom";
 function ShowMap () {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const navigate = useNavigate()
+
+    const lat = searchParams.get('lat');
+    const lng = searchParams.get('lng');
+    console.log(lat, lng);
+
     return (
-        <div className={styles.mapContainer}>Map</div>
+        <div className={styles.mapContainer} onClick={() => navigate("form")} role='button'>
+            <h1>Map</h1>
+            <h1>Position: {lat} {lng}</h1>
+            <button onClick={() => setSearchParams({ lat: 23, lng: 30 })}>Change Position</button>
+        </div>
     )
 }
 
