8c25805ca21c606f5b27ff8f23053dd679bc04a3
srikanth019 <srikanth.golla@brainvire.com>
2024-05-02 15:15:11 +0530

    Complete Drink-water app till Chalange

--- drinkig/src/App.js
+++ drinkig/src/App.js
@@ -1,36 +1,67 @@
-import { useState } from "react"
+import "./app.css";
+import React, { useState } from "react";
+
+const goal = 2;
+const totalCups = 8;
+const width = "165px";
+const height = "350px";
+
 function App () {
-
   const [selectedJar, setSelectedJar] = useState(null);
+  const [remaining, setRemaining] = useState(goal);
+  const [percentage, setPercentage] = useState(0);
+  const [jarPercentageHeight, setJarPercentageHeight] = useState("0px");
 
-  // Function to handle clicking on a jar
+  const isPercentageVisible = jarPercentageHeight !== "0px";
+
   const handleJarClick = (index) => {
-
-    if (selectedJar === index) {
-      setSelectedJar(index - 1)
+    if (index === selectedJar) {
+      setSelectedJar(index - 1);
+      const jarHeight = `${((index - 1) / totalCups) * 350}px`;
+      setJarPercentageHeight(jarHeight);
+      setPercentage(((index - 1) / totalCups) * 100);
+      setRemaining(goal - (index - 1) * 0.25);
     } else {
       setSelectedJar(index);
+      const jarHeight = `${(index / totalCups) * 350}px`;
+      setJarPercentageHeight(jarHeight);
+      setPercentage((index / totalCups) * 100);
+      setRemaining(goal - index * 0.25);
     }
   };
 
+
+
   return (
-    <div className="bg-blue-400 text-white font-sans-serif flex flex-col items-center h-screen p-10">
+    <div className="text-white font-sans-serif flex flex-col items-center h-screen p-10">
       <div className="text-center">
         <div className="text-2xl mb-2 p-2 font-semibold">Drink Water</div>
-        <div className="text-xl ">Goal: 2 Liters</div>
-        <div className="w-[165Px] h-[350px] bg-slate-50 border-[4px] text-blue-500 border-[#3456ce] m-3 rounded-t-none rounded-b-xl flex items-center justify-center">
-          <div class="remained" id="remained">
-            <span id="liters">2 L</span>
-            <small> Remained</small>
+        <div className="text-xl">Goal: {goal} Liters</div>
+        <div
+          className="bg-slate-50 border-[4px] text-blue-500 border-[#3456ce] m-3 rounded-t-md rounded-b-xl flex flex-col   items-center justify-center"
+          style={{ width, height }}
+        >
+          {
+            remaining > 0 ?
+              <div className="remained" id="remained" style={{ height: "0px" }}>
+                <span id="liters" className="font-semibold">{remaining} L</span>
+                <small> Remained</small>
+              </div>
+              : ""
+          }
+          <div
+            className={`bg-blue-300 flex items-center justify-center font-bold text-3xl rounded-t-md rounded-b-xl`}
+            id="percentage"
+            style={{ visibility: isPercentageVisible ? "visible" : "hidden", height: jarPercentageHeight, width: "158px" }}
+          >
+            {percentage} %
           </div>
-
-          <div class="percentage" id="percentage"></div>
         </div>
       </div>
       <div className="mt-6">
         <div className="text-xl">Select how many glasses of water you have drank:</div>
         <div className="grid grid-cols-4 gap-4 m-4 ml-7">
-          {Array.from({ length: 8 }, (_, i) => (
+          {Array.from({ length: totalCups }, (_, i) => (
             <Jar
               key={i + 1}
               index={i + 1}
@@ -44,13 +75,13 @@ function App () {
   );
 }
 
-export default App
+export default App;
 
 function Jar ({ isSelected, handleClick }) {
   return (
-    <div className={`w-[60px] h-[100px] ${isSelected ? 'bg-blue-400 text-white' : 'bg-white text-blue-400'
-      } text-center p-3 pt-6 rounded-b-xl border-4 border-[#3456ce]`} onClick={handleClick}>
+    <div className={`w-[60px] h-[100px] ${isSelected ? 'bg-blue-400 text-white' : 'bg-white text-blue-400'} text-center p-3 pt-6 rounded-b-xl border-4 border-[#3456ce]`} onClick={handleClick}>
       250 ml
     </div>
-  )
+  );
 }
+
