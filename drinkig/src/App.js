51ac000adf98e6c3952803e06c325c65d44ec115
srikanth019 <srikanth.golla@brainvire.com>
2024-05-02 16:37:58 +0530

    drinking water with dynamic values

--- drinkig/src/App.js
+++ drinkig/src/App.js
@@ -1,8 +1,13 @@
 import "./app.css";
 import React, { useState } from "react";
 
-const goal = 2;
+const capacity = 2;
 const totalCups = 8;
+
+const capacityOfCup = (capacity / totalCups).toFixed(2)
+
+//Water per cup in ML
+const waterPerCup = ((capacity * 1000) / totalCups).toFixed(2);
 
 // width and height are in px format
 const width = 165;
@@ -10,25 +15,37 @@ const height = 350;
 
 function App () {
   const [selectedJar, setSelectedJar] = useState(null);
-  const [remaining, setRemaining] = useState(goal);
+  const [remaining, setRemaining] = useState(capacity);
   const [percentage, setPercentage] = useState(0);
   const [waterPercentage, setWaterPercentage] = useState(0);
 
-  const isPercentageVisible = waterPercentage !== 0;
-
   const handleJarClick = (index) => {
-    if (index === selectedJar) {
+    // without rounding values
+    /*if (index === selectedJar) {
       setSelectedJar(index - 1);
-      const jarHeight = ((index - 1) / totalCups) * height;
-      setWaterPercentage(jarHeight);
+      const waterLevel = ((index - 1) / totalCups) * height;
+      setWaterPercentage(waterLevel);
       setPercentage(((index - 1) / totalCups) * 100);
-      setRemaining(goal - (index - 1) * 0.25);
+      setRemaining(capacity - (index - 1) * capacityOfCup);
     } else {
       setSelectedJar(index);
-      const jarHeight = (index / totalCups) * height;
-      setWaterPercentage(jarHeight);
+      const waterLevel = (index / totalCups) * height;
+      setWaterPercentage(waterLevel);
       setPercentage((index / totalCups) * 100);
-      setRemaining(goal - index * 0.25);
+      setRemaining(capacity - index * capacityOfCup);
+    }*/
+    if (index === selectedJar) {
+      setSelectedJar(index - 1);
+      const waterLevel = Math.round(((index - 1) / totalCups) * height * 100) / 100;
+      setWaterPercentage(waterLevel.toFixed(2));
+      setPercentage(Math.round(((index - 1) / totalCups) * 100 * 100) / 100);
+      setRemaining((capacity - (index - 1) * capacityOfCup).toFixed(2));
+    } else {
+      setSelectedJar(index);
+      const waterLevel = Math.round((index / totalCups) * height * 100) / 100;
+      setWaterPercentage(waterLevel.toFixed(2));
+      setPercentage(Math.round((index / totalCups) * 100 * 100) / 100);
+      setRemaining((capacity - index * capacityOfCup).toFixed(2));
     }
   };
 
@@ -38,9 +55,9 @@ function App () {
     <div className="text-white font-sans-serif flex flex-col items-center h-screen p-10">
       <div className="text-center">
         <div className="text-2xl mb-2 p-2 font-semibold">Drink Water</div>
-        <div className="text-xl">Goal: {goal} Liters</div>
+        <div className="text-xl">Capacity: {capacity} Liters</div>
         <div
-          className="bg-slate-50 border-[4px] text-blue-500 border-[#3456ce] m-3 rounded-t-md rounded-b-xl flex flex-col   items-center justify-center"
+          className="bg-slate-50 border-[4px] text-blue-500 border-[#3456ce] m-3 rounded-t-md rounded-b-3xl flex flex-col   items-center justify-center"
           style={{ width: `${width}px`, height: `${height}px` }}
         >
           {
@@ -51,13 +68,14 @@ function App () {
               </div>
               : ""
           }
-          <div
-            className={`bg-blue-300 flex items-center justify-center font-bold text-3xl rounded-t-md rounded-b-xl`}
+          {percentage > 0 ? <div
+            className={`bg-blue-300 flex items-center justify-center font-bold text-3xl rounded-t-md rounded-b-3xl`}
             id="percentage"
-            style={{ visibility: isPercentageVisible ? "visible" : "hidden", height: `${waterPercentage}px`, width: `${width - 7}px` }} //need to decrease width approx 7px
+            style={{ height: `${waterPercentage}px`, width: `${width - 7}px` }} //need to decrease width approx 7px
           >
             {percentage} %
-          </div>
+          </div> : ""
+          }
         </div>
       </div>
       <div className="mt-6">
@@ -81,8 +99,8 @@ export default App;
 
 function Jar ({ isSelected, handleClick }) {
   return (
-    <div className={`w-[60px] h-[100px] ${isSelected ? 'bg-blue-400 text-white' : 'bg-white text-blue-400'} text-center p-3 pt-6 rounded-b-xl border-4 border-[#3456ce]`} onClick={handleClick}>
-      250 ml
+    <div className={`w-[75px] h-[100px] ${isSelected ? 'bg-blue-300 text-white' : 'bg-white text-blue-500'} text-center p-2 pt-6 rounded-b-2xl rounded-t-md border-4 border-[#3456ce]`} onClick={handleClick}>
+      {waterPerCup} ml
     </div>
   );
 }
